2c96fd7eaef77ec1da83dd123a82ec40d3a7d9d3
M Daniyal Sahito <160993870+M-Daniyal-Sahito@users.noreply.github.com>
2024-04-23 15:44:29 +0500

    Add files via upload

---
+++ game.ts
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+import inquirer from "inquirer"
+const number=Math.floor(Math.random()*6+1)
+const answer= await inquirer.prompt([{
+    name:"rendomNumber",
+    type:"number",
+    message: "Please guess the number between 1 to 6"
+}])
+if (answer.rendomNumber===number){
+    console.log("You are the winner you guess the correct number");
+    
+} 
+else {console.log("You are the loser you guess the wrong number");
+}
